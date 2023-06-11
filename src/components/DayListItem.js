762f20744b46cb9bcdd328fc0c07144c506ab324
Anastasia <z.anastasia11@gmail.com>
2023-06-11 15:56:01 -0700

    create DayListItem storybook and add props to the compomemt

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,15 @@
 import React from "react";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list-item", {
+    "day-list-item--selected": props.selected
+  });
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={dayClass} onClick={() => {props.setDay(props.name)}} >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
 }
