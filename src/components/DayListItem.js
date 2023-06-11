7bb1acf884ca47c1eebd74808f01789cff3e88dc
Anastasia <z.anastasia11@gmail.com>
2023-06-11 16:08:42 -0700

    style the DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,9 +1,11 @@
 import React from "react";
 import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  const dayClass = classNames("day-list-item", {
-    "day-list-item--selected": props.selected
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
   });
 
   return (
