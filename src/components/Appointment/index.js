ef4b50645ef1f98be4a88e39fbcab4925b8b05af
Anastasia <z.anastasia11@gmail.com>
2023-06-13 14:52:11 -0700

    Import Empty.js into index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./styles.scss";
 
+import Header from "./Header.js";
+import Empty from "./Empty.js";
+
+
 export default function Appointment(props) {
 
   return (
