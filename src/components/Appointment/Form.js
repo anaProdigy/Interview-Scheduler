a10c13b21349355aa16fdeddef1cc29959c7d4a0
Anastasia <z.anastasia11@gmail.com>
2023-06-13 17:08:26 -0700

    Add a cancel function to the <Form> component that calls reset() and props.onCancel.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -9,6 +9,18 @@ export default function Form(props) {
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
 
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -30,7 +42,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel} >Cancel</Button>
+          <Button danger onClick={cancel} >Cancel</Button>
           <Button confirm onClick={props.onSave} >Save</Button>
         </section>
       </section>
