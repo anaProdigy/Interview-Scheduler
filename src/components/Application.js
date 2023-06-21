a0402ecacfe720cf809c312eb7e1825e2bb3155a
Anastasia <z.anastasia11@gmail.com>
2023-06-21 14:59:06 -0700

    use useState to add a days state to the our Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,12 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
-
-import Appointment from "components/Appointment";
 //same as =>
 // import Appointment from "components/Appointment/index";
+import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 const appointments = {
   "1": {
@@ -68,6 +51,15 @@ const appointments = {
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days")
+    .then((response) => {
+     setDays([...response.data])
+    })
+  }, [])
+  
 
   const appointmentsList = Object.values(appointments).map(appointment => (
     <Appointment
