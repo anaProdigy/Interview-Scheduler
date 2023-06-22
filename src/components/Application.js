db50ad604e3c915d15612bd72284ddd4704c80ec
Anastasia <z.anastasia11@gmail.com>
2023-06-22 09:43:51 -0700

    Update the Application component to use Promise.all to make requests to the days and then appointments endpoints in the existing effect method and console.log the response.

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,44 +8,44 @@ import Appointment from "components/Appointment";
 
 
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+// const appointments = {
+//   "1": {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   "2": {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   "3": {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   "4": {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer: {
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   "5": {
+//     id: 5,
+//     time: "4pm",
+//   }
+// };
 
 
 export default function Application(props) {
@@ -61,22 +61,32 @@ export default function Application(props) {
     // you may put the line below, but will have to remove/comment hardcoded appointments variable
     appointments: {}
   });
-  console.log("line 64", state)
+  
+  const dailyAppointments = [];
 
+//????????????????????????????????????????????????
   const setDay = day => setState({ ...state, day });
-  const setDays = (days) => {
-    setState(prev => ({ ...prev, days }));
-  }
+ 
 
   useEffect(() => {
-    axios.get("/api/days")
-    .then((response) => {
-     setDays([...response.data])
-    })
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      // axios.get("/api/interviewrs")
+    ]).then((all) => {
+      console.log(all); // first
+      // console.log(all[1]); // second
+      // console.log(all[2]); // third
+      setState(prev => ({ ...prev, days: all[0].data,appointments: all[1].data }));
+    });
+
+
+   
   }, [])
   
 
-  const appointmentsList = Object.values(appointments).map(appointment => (
+  const appointmentsList = dailyAppointments.map(appointment => (
     <Appointment
        key={appointment.id}
       {...appointment} 
