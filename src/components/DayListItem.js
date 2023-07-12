756ff9f1173f5d099f93952a0ed8e43cfd4cd5e1
Anastasia <z.anastasia11@gmail.com>
2023-07-11 19:01:52 -0700

    add test file for appointment

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -22,7 +22,12 @@ export default function DayListItem(props) {
 
   return (
    
-    <li data-testid="day" className={dayClass} onClick={props.setDay} selected={props.selected} >
+    <li
+     data-testid="day" 
+     className={dayClass} 
+     onClick={props.setDay} 
+     selected={props.selected}
+     >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light"> {formatSpots()} </h3>
     </li>
